646ca06080093bfbefce0449df8622db06fa0685
PinkiePie-tech <62440844+PinkiePie-tech@users.noreply.github.com>
2023-01-12 16:41:32 +0100

    2 files modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,41 +11,9 @@ export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
   constructor(private pokeService: PokeService) {
-    // this.pokeService
-    //   .getPokemon()
-    //   .pipe(take(1))
-    //   .subscribe((val) => console.log('getPokemon', val));
-    // this.pokeService
-    //   .getPokemonByName('bulbasaur')
-    //   .pipe(take(1))
-    //   .subscribe((val) => console.log('getPokemonByName', val));
-    // this.pokeService
-    //   .getPokemonById(136)
-    //   .pipe(take(1))
-    //   .subscribe((val) => console.log('getPokemonById', val));
-    // this.pokeService
-    //   .getPokemonSorted('asc')
-    //   .pipe(take(1))
-    //   .subscribe((val) => console.log('getPokemonSorted Asc', val));
-    // this.pokeService
-    //   .getPokemonSorted('desc')
-    //   .pipe(take(1))
-    //   .subscribe((val) => console.log('getPokemonSorted Desc', val));
-    // this.pokeService
-    //   .getPokemonSorted()
-    //   .pipe(take(1))
-    //   .subscribe((val) => console.log('getPokemonSorted No Params', val));
-    // this.pokeService
-    //   .getShortInfoByName('bulbasaur')
-    //   .pipe(take(1))
-    //   .subscribe((val) => console.log('getShortInfoByName', val));
-    // this.pokeService
-    //   .getPokemonAbilities()
-    //   .pipe(take(1))
-    //   .subscribe((val) => console.log('getPokemonAbilities', val));
     this.pokeService
-      .getPokemonAbilityByName('stench')
+      .getAnObjectWithUrl('wartortle')
       .pipe(take(1))
-      .subscribe((val) => console.log('getPokemonAbilityByName', val));
+      .subscribe((val) => console.log('getAnObjectWithUrl', val));
   }
 }
--- src/app/shared/services/poke.services.ts
+++ src/app/shared/services/poke.services.ts
@@ -28,106 +28,15 @@ export class PokeService {
    * le `return of()` est juste présent pour que tu puisses voir les résultats, un fois la fonction codé, tu devras supprimer cette ligne de code pour voir le résultat apparaitre dans la console
    */
 
-  public getPokemon(): Observable<IPokemon[]> {
-    /** l'url pour cette api sera la suivante : 'https://pokeapi.co/api/v2/pokemon' */
-    return this.httpClient
-      .get<IPokeList>('https://pokeapi.co/api/v2/pokemon')
-      .pipe(map((val) => val.results));
-  }
-
-  public getPokemonByName(name: string): Observable<IPokemonDetail> {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/pokemon/${name}` */
-    return this.httpClient.get<IPokemonDetail>(
-      `https://pokeapi.co/api/v2/pokemon/${name}`
-    );
-  }
-
-  public getPokemonById(id: number): Observable<IPokemonDetail> {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/pokemon/${number}` */
-    return this.httpClient.get<IPokemonDetail>(
-      `https://pokeapi.co/api/v2/pokemon/${id}`
-    );
-  }
-
-  public getPokemonSorted(sort?: 'asc' | 'desc'): Observable<IPokemon[]> {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/pokemon`
-     * un paramètre a été ajouté, "sort" pourra être ascendant ou descendant, en fonction de la valeur qu'il prendra, les deux valeurs possible de "sort" sont 'asc' et 'desc'
-     * Tu dois donc créer une condition pour transformer la donnée reçu et la trier en fonction de la valeur de sort, si aucune valeur n'est précisée, alors tu devras renvoyer le tableau comme il a été reçu
-     */
-    return this.getPokemon().pipe(
-      map((val) => {
-        if (!sort) {
-          return val;
-        }
-        return val.sort((a, b) =>
-          sort === 'asc'
-            ? a.name.localeCompare(b.name)
-            : b.name.localeCompare(a.name)
-        );
-      })
-    );
-  }
-
-  public getShortInfoByName(name: string): Observable<IPokemonShortDetail> {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/pokemon/${name}`, le but sera de transformer la donnée pour ne prendre que l'essentiel, l'interface proposée a déjà été détaillée */
-    return this.getPokemonByName(name).pipe(
-      map((pokemon: IPokemonDetail) => {
-        return {
-          id: pokemon.id,
-          name: pokemon.name,
-          weight: pokemon.weight,
-          height: pokemon.height,
-          species: pokemon.species.name,
-          types: pokemon.types.map((type: IPokemonType) => type.type.name),
-        } as IPokemonShortDetail;
-      })
-    );
-  }
-
-  public getPokemonAbilities(): Observable<IAbility[]> {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/ability`, aucune interface n'a été créé pour cette route, il faudra donc remplacer "any" par le type que tu auras créé, ce type correspondra exactement au retour de l'api*/
-    return this.httpClient
-      .get<IAbilityList>('https://pokeapi.co/api/v2/ability')
-      .pipe(map((val) => val.results));
-  }
-
-  public getPokemonAbilityByName(name: string) {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/ability/${name}`, aucune interface n'a été créé pour cette route, il faudra donc remplacer "any" par le type que tu auras créé, ce type correspondra exactement au retour de l'api
-     */
-    return this.httpClient
-      .get<IAbilityDetail>(`https://pokeapi.co/api/v2/ability/${name}`)
-      .pipe(
-        map((response: IAbilityDetail) => {
-          console.log('res', response);
-          return response.pokemon;
-        }),
-        map((suite: IAbilityPokemon[]) => {
-          console.log('suite', suite);
-          return suite.map((value: IAbilityPokemon) => {
-            return {
-              name: value.pokemon.name,
-              slot: value.slot,
-            };
-          });
-        }),
-        map((finish: { name: string; slot: number }[]) => {
-          console.log('finish', finish);
-          return finish.sort((a, b) => a.slot - b.slot);
-        })
-      );
-  }
-
   public getAnObjectWithUrl(name: string) {
     /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/ability/${name}`, aucune interface n'a été créé pour cette route, il faudra donc remplacer "any" par le type que tu auras créé, ce type correspondra exactement au retour de l'api
      */
-    return this.httpClient.get<IPokemonDetail>(
-      `https://pokeapi.co/api/v2/pokemon/${name}`
-    ).pipe(
-      map((value: any) => {
-        return {
-          
-        }
-      })
-    );
+    return this.httpClient
+      .get<IPokemonDetail>(`https://pokeapi.co/api/v2/pokemon/${name}`)
+      .pipe(
+        map((value: any) => {
+          return value;
+        })
+      );
   }
 }
