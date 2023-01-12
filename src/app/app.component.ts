7389684df69fdcc81d8bf00d1343bdc35383768c
PinkiePie-tech <62440844+PinkiePie-tech@users.noreply.github.com>
2023-01-12 16:57:50 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,17 +11,17 @@ export class AppComponent {
   name = 'Angular ' + VERSION.major;
 
   constructor(private pokeService: PokeService) {
-    this.pokeService
-      .getAnObjectWithUrl('wartortle')
-      .pipe(take(1))
-      .subscribe((val) => console.log('getAnObjectWithUrl', val));
-    this.pokeService
-      .getAnObjectWithUrlRandom('wartortle')
-      .pipe(take(1))
-      .subscribe((val) => console.log('getAnObjectWithUrlRandom', val));
-    this.pokeService
-      .getAListByUrl()
-      .pipe(take(1))
-      .subscribe((val) => console.log('getAListByUrl', val));
+    // this.pokeService
+    //   .getAnObjectWithUrl('wartortle')
+    //   .pipe(take(1))
+    //   .subscribe((val) => console.log('getAnObjectWithUrl', val));
+    // this.pokeService
+    //   .getAnObjectWithUrlRandom('wartortle')
+    //   .pipe(take(1))
+    //   .subscribe((val) => console.log('getAnObjectWithUrlRandom', val));
+    // this.pokeService
+    //   .getAListByUrl()
+    //   .pipe(take(1))
+    //   .subscribe((val) => console.log('getAListByUrl', val));
   }
 }
