2195efe445ac1d16734c55fe235d8af8539425d3
PinkiePie-tech <62440844+PinkiePie-tech@users.noreply.github.com>
2023-01-12 16:57:24 +0100

    1 file modified

--- src/app/shared/services/poke.services.ts
+++ src/app/shared/services/poke.services.ts
@@ -20,17 +20,7 @@ import {
 export class PokeService {
   constructor(private httpClient: HttpClient) {}
 
-  /**
-   * Toutes les fonctions vont provenir de la web api https://pokeapi.co/api/v2, on change d'API histoire de varier les plaisirs
-   * Tu auras à créer les modèles des réponses http de l'API, et dans certains cas, les interfaces de sorties, et dans d'autre cas, un model dont que je t'aurai construit :D
-   * Pour chaque exercice, je te donnerai l'url, cette url contiendra parfois des variables qui seront en en-têtes de la fonction
-   * Ton code devra être contenu dans la fonction qui lui es associée, des interfaces seront manquante dans certain cas, il sera à ta charge de les écrire
-   * le `return of()` est juste présent pour que tu puisses voir les résultats, un fois la fonction codé, tu devras supprimer cette ligne de code pour voir le résultat apparaitre dans la console
-   */
-
   public getAnObjectWithUrl(name: string) {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/ability/${name}`, aucune interface n'a été créé pour cette route, il faudra donc remplacer "any" par le type que tu auras créé, ce type correspondra exactement au retour de l'api
-     */
     return this.httpClient
       .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
       .pipe(
@@ -60,8 +50,6 @@ export class PokeService {
   }
 
   public getAnObjectWithUrlRandom(name: string) {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/ability/${name}`, aucune interface n'a été créé pour cette route, il faudra donc remplacer "any" par le type que tu auras créé, ce type correspondra exactement au retour de l'api
-     */
     return this.httpClient
       .get(`https://pokeapi.co/api/v2/pokemon/${name}`)
       .pipe(
@@ -89,10 +77,8 @@ export class PokeService {
   }
 
   public getAListByUrl() {
-    /** l'url pour cette api sera la suivante : `https://pokeapi.co/api/v2/ability/${name}`, aucune interface n'a été créé pour cette route, il faudra donc remplacer "any" par le type que tu auras créé, ce type correspondra exactement au retour de l'api
-     */
     return this.httpClient.get(`https://pokeapi.co/api/v2/pokemon/`).pipe(
-      map((value: IPokeList) => {
+      map((value: any) => {
         return value.results;
       }),
       map((value) => {
