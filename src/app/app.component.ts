97ec48cb31e0fb1c9180443117e16a6fbded8e9f
PinkiePie-tech <62440844+PinkiePie-tech@users.noreply.github.com>
2023-01-13 14:07:05 +0100

    1 file modified

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,18 +12,6 @@ export class AppComponent {
 
   constructor(private pokeService: PokeService) {
     this.pokeService
-      .getAnObjectWithUrl('wartortle')
-      .pipe(take(1))
-      .subscribe((val) => console.log('getAnObjectWithUrl', val));
-    this.pokeService
-      .getAnObjectWithUrlRandom('wartortle')
-      .pipe(take(1))
-      .subscribe((val) => console.log('getAnObjectWithUrlRandom', val));
-    this.pokeService
-      .getAListByUrl()
-      .pipe(take(1))
-      .subscribe((val) => console.log('getAListByUrl', val));
-    this.pokeService
       .hardcoreObservable()
       .pipe(take(1))
       .subscribe((val) => console.log('hardcoreObservable', val));
