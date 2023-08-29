88b0fafbf6f01a80caef8acdd60a9d5dd38aba39
Marion-tech <cainael@gmail.com>
2023-08-29 12:58:09 +0200

    Exo chats switch & combine

--- src/app/exercice/exercice.component.ts
+++ src/app/exercice/exercice.component.ts
@@ -1,22 +1,66 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { FormControl } from "@angular/forms";
+import { Observable, combineLatest, map, switchMap } from "rxjs";
+import { ICat } from "../shared/models/cat.interface";
+import { CatService } from "../shared/services/cat.services";
 
 @Component({
-  selector: 'app-exercice',
-  templateUrl: './exercice.component.html',
-  styleUrls: ['./exercice.component.css']
+  selector: "app-exercice",
+  templateUrl: "./exercice.component.html",
+  styleUrls: ["./exercice.component.css"],
 })
 export class ExerciceComponent {
+  public fcSelect = new FormControl<number[]>([]);
+  public cats$: Observable<ICat[]>;
+  public catSelected$: Observable<ICat[]>;
 
-  constructor() {
+  constructor(private CatService: CatService) {
+    //EXO-1
+    this.cats$ = this.CatService.getCats();
+    /*this.fcSelect.valueChanges.subscribe((idCat: number | null) =>
+      console.log(idCat)
+    );*/
+    //EXO-1
+    //
+    //EXO-2
+    //   this.catSelected$ = this.fcSelect.valueChanges.pipe(
+    //     switchMap((idCat: number | null) => {
+    //       return this.CatService.getCatById(idCat as number);
+    //     })
+    //   );
+    //   this.catSelected$.subscribe((cat: ICat | undefined) =>
+    //     console.log("chat: ", cat)
+    //   );
+    // }
+    let listeCat: ICat[] | undefined = [];
 
+    /*this.catSelected$ = this.fcSelect.valueChanges.pipe(
+    switchMap((idCat:number)=>{ 
+      listeCat.push(this.CatService.getCatById(idCat as number));
+      return listeCat;
+    })
+  )*/
+
+    this.catSelected$ = combineLatest([
+      this.fcSelect.valueChanges,
+      this.cats$,
+    ]).pipe(
+      map(([select, liste]: [number[] | null, ICat[]]) => {
+        return liste.filter((cat: ICat) => {
+          return select?.find((id: number) => id === cat.id);
+        });
+      })
+    );
+    // Voir la partie service et mettre en place des mocks de donnée, une liste des data est disponible dans le fichier cat
+    // une fois les services mise en place, nous allons revenir sur ce composant
+
+    // Etape 1 : Une fois les services mise en place ( tu peux me demander de l'aide, je suis à côté de toi,
+    // juste à ta gauche ), créer un select permettant de sélectionner les cats, ce select retournera pour
+    // valeur l'id du cat qu'il aura sélectionné
+
+    // Etape 2 : Créer un observable qui, en fonction de la valeur dans le select, retourne
+    //le chat qui correspond à cette valeur. afficher le chat dans la partie html
+
+    // Etape 3 : Passer le select en multiple
   }
-  
-  // Voir la partie service et mettre en place des mocks de donnée, une liste des data est disponible dans le fichier cat
-  // une fois les services mise en place, nous allons revenir sur ce composant
-
-  // Etape 1 : Une fois les services mise en place ( tu peux me demander de l'aide, je suis à côté de toi, juste à ta gauche ), créer un select permettant de sélectionner les cats, ce select retournera pour valeur l'id du cat qu'il aura sélectionné
-
-  // Etape 2 : Créer un observable qui, en fonction de la valeur dans le select, retourne le chat qui correspond à cette valeur. afficher le chat dans la partie html
-
-  // Etape 3 : Passer le select en multiple
 }
--- src/app/shared/services/cat.services.ts
+++ src/app/shared/services/cat.services.ts
@@ -1,27 +1,41 @@
-
-import { Injectable } from '@angular/core';
+import { Injectable } from "@angular/core";
+import { Observable, map, switchMap, timer } from "rxjs";
+import { ICat } from "../models/cat.interface";
+import { dataCat } from "../data/cat.data";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class CatService {
   constructor() {}
 
   // Première étape, création d'un mock de service
 
-  public getCats() {
-    /** 
+  public getCats(): Observable<ICat[]> {
+    /**
      * remplir cette méthode qui va nous retourner une liste de Cat, la liste est stocké aujourd'hui en local,
      * pour simuler une requête réseau, nous devons ajouter un délai de 200 ms à cette requête
-     */ 
-    return ;
+     */
+
+    return timer(200).pipe(
+      map((num: number) => {
+        return dataCat;
+      })
+    );
   }
 
-  public getCatById(id: number) {
-    /** 
+  public getCatById(id: number | null): Observable<ICat | undefined> {
+    /**
      * remplir cette méthode qui va nous retourner un Cat en fonction d'un ID, la liste est stocké aujourd'hui en local,
      * pour simuler une requête réseau, nous devons ajouter un délai de 100 ms à cette requête
-     */ 
-    return ;
+     */
+
+    return timer(100).pipe(
+      map((num: number) => {
+        return dataCat.find((cat: ICat) => {
+          return cat.id == id;
+        });
+      })
+    );
   }
 }
