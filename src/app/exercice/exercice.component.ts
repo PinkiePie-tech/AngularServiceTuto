e08dd1e0ac60bdfb5501f3e9ae04e3a9602c188a
Marion-tech <cainael@gmail.com>
2023-08-29 14:05:25 +0200

    service exo catsize

--- src/app/exercice/exercice.component.ts
+++ src/app/exercice/exercice.component.ts
@@ -1,25 +1,25 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
 @Component({
-  selector: 'app-exercice',
-  templateUrl: './exercice.component.html',
-  styleUrls: ['./exercice.component.css']
+  selector: "app-exercice",
+  templateUrl: "./exercice.component.html",
+  styleUrls: ["./exercice.component.css"],
 })
 export class ExerciceComponent {
+  constructor() {}
 
-  constructor() {
-
-  }
-  
   // Voir la partie service et mettre en place des mocks de donnée, une liste des data est disponible dans le fichier cat
   // une fois les services mise en place, nous allons revenir sur ce composant
 
-  // Etape 1 : Une fois les services mise en place ( tu peux me demander de l'aide, je suis à côté de toi, juste à ta gauche ), créer un select permettant de sélectionner 
-  // les categories, ce select affichera les chats qui correspondent à la catégorie des deux
+  // Etape 1 : Une fois les services mise en place ( tu peux me demander de l'aide,
+  // je suis à côté de toi, juste à ta gauche ), créer un select permettant de sélectionner
+  // les categories, ce select affichera les chats qui correspondent à la catégorie des
+  // deux
 
-  // Etape 2 : créer une input permettant de faire une recherche dans la liste, cette input se basera sur le nom de la race de chat
+  // Etape 2 : créer un input permettant de faire une recherche dans la
+  //liste, cet input se basera sur le nom de la race de chat
 
   // Etape 3 : Fusionner le choix de la catégorie et l'input permettant de faire la recherche
 
-  // Etape 4 : Ajouter l'information dans l'affichage sur la size des chats 
+  // Etape 4 : Ajouter l'information dans l'affichage html sur la size des chats
 }
--- src/app/shared/services/cat.services.ts
+++ src/app/shared/services/cat.services.ts
@@ -1,48 +1,71 @@
-
-import { Injectable } from '@angular/core';
-import { dataCat } from '../data/cat.data';
-import { Observable, of } from 'rxjs';
+import { Injectable } from "@angular/core";
+import { dataCat, dataCatSize } from "../data/cat.data";
+import { Observable, map, of, timer } from "rxjs";
+import { ICat } from "../models/cat.interface";
+import { ICatSize } from "../models/catSize.interface";
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: "root",
 })
 export class CatService {
   constructor() {}
 
   // Première étape, création d'un mock de service
 
-  public getCats() {
-    /** 
+  public getCats(): Observable<ICat[]> {
+    /**
      * remplir cette méthode qui va nous retourner une liste de Cat, la liste est stocké aujourd'hui en local,
      * pour simuler une requête réseau, nous devons ajouter un délai de 200 ms à cette requête
-     */ 
-    return ;
+     */
+    return timer(200).pipe(
+      map((timer: number) => {
+        return dataCat;
+      })
+    );
   }
 
-  public getCatById(id: number) {
-    /** 
+  public getCatById(id: number): Observable<ICat | undefined> {
+    /**
      * remplir cette méthode qui va nous retourner un Cat en fonction d'un ID, la liste est stocké aujourd'hui en local,
      * pour simuler une requête réseau, nous devons ajouter un délai de 100 ms à cette requête
-     */ 
-    return ;
+     */
+    return timer(100).pipe(
+      map((timer: number) => {
+        return dataCat.find((cat: ICat) => {
+          return cat.id === id;
+        });
+      })
+    );
   }
 
-  public getCatSizes() {
-    /** 
+  public getCatSizes(): Observable<ICatSize[]> {
+    /**
      * remplir cette méthode qui va nous retourner une liste de CatSize, la liste est stocké aujourd'hui en local,
      * pour simuler une requête réseau, nous devons ajouter un délai de 200 ms à cette requête
-     */ 
+     */
+
+    return timer(200).pipe(
+      map((timer: number) => {
+        return dataCatSize;
+      })
+    );
   }
 
-  public getCatSizesById(id: number) {
-    /** 
+  public getCatSizesById(id: number): Observable<ICatSize | undefined> {
+    /**
      * remplir cette méthode qui va nous retourner un CatSize en fonction d'un ID, la liste est stocké aujourd'hui en local,
      * pour simuler une requête réseau, nous devons ajouter un délai de 100 ms à cette requête
-     */ 
-    return ;
+     */
+    return timer(200).pipe(
+      map((timer: number) => {
+        return dataCatSize.find((size: ICatSize) => {
+          return size.id === id;
+        });
+      })
+    );
   }
 
   public getCategory(): Observable<string[]> {
-    return of(['A', 'B', 'C'])
+    return of(["A", "B", "C"]);
   }
 }
